5d357898c287b19d02e0e7eef705469afbd48069
VISWA7827 <viswanagumalli3221>
2026-08-28 21:49:41 +0530

    Check Student Details

--- CheckDetails.c
+++ CheckDetails.c
@@ -13,18 +13,22 @@ int CheckDetails()
         }
         
     }
+    //Check the Student Adhaar Number 
     if(1000000000000<Students[TotalStudents].AdhaarNo||99999999999>Students[TotalStudents].AdhaarNo)
     {
         return 1;
     }
+     //Check the Student Father Adhaar Number 
     if(1000000000000<Students[TotalStudents].FatherAdhaarNo||99999999999>Students[TotalStudents].FatherAdhaarNo)
     {
         return 2;
     }
+    //Check the Student Father phone Number
     if(10000000000<Students[TotalStudents].PhoneNo||999999999>Students[TotalStudents].PhoneNo)
     {
         return 3;
     }
+    //Check Student age to permit
     if(Students[TotalStudents].Stuclass<=0||Students[TotalStudents].Stuclass>=11)
     {
         return 4;
@@ -37,6 +41,7 @@ int CheckDetails()
             return 5;
         }
     }
+    //Check the Gender
     if(Students[TotalStudents].gender=='f'||Students[TotalStudents].gender=='F'||Students[TotalStudents].gender=='m'||Students[TotalStudents].gender=='M')
     {
         return 0;
@@ -45,6 +50,7 @@ int CheckDetails()
     {
         return 6;
     }
+    //check the Student And Father Number is Same or not
     if(Students[TotalStudents].AdhaarNo==Students[TotalStudents].FatherAdhaarNo)
     {
         return 7;
