da45df309e9904e0e35fccd0fdec6edc9ef5b36c
VISWA7827 <viswanagumalli3221>
2026-08-28 21:22:52 +0530

    Enter Student Details

--- StudentAdd.c
+++ StudentAdd.c
@@ -6,32 +6,33 @@
 void StudentAdd()
 {
     if(TotalStudents==-1){
-        Students=malloc(1*sizeof(STUDENT));
+        Students=malloc(1*sizeof(STUDENT));//Allocating memory to Student Structure
     }
     else
     {
-        Students=(STUDENT*)realloc(Students,(TotalStudents+2)*sizeof(STUDENT));
+        Students=(STUDENT*)realloc(Students,(TotalStudents+2)*sizeof(STUDENT));//Allocating memory to Student Structure
     }
+    printf("!!!Enter the Correct Details of the Student!!!");
     printf("Enter the Adhaar NUmber:");
-    scanf("%lld",&Students[TotalStudents].AdhaarNo);
+    scanf("%lld",&Students[TotalStudents].AdhaarNo);//Assigning Adhaar Student Number
     printf("Enter the Surname:");
-    scanf("%s",Students[TotalStudents].Surname);
+    scanf("%s",Students[TotalStudents].Surname);//Assigning Surname of Student
     printf("Enter the Student Name:");
-    scanf(" %[^\n]",Students[TotalStudents].StudentName);
+    scanf(" %[^\n]",Students[TotalStudents].StudentName);//Assign Student Name
     printf("Enter the Father Name:");
-    scanf(" %[^\n]",Students[TotalStudents].FatherName);
+    scanf(" %[^\n]",Students[TotalStudents].FatherName);//Assign Student Father Name
     printf("Enter the Father Adhaar Number:");
-    scanf("%lld",&Students[TotalStudents].FatherAdhaarNo);
+    scanf("%lld",&Students[TotalStudents].FatherAdhaarNo);//Assign Student Father Adhaar Number
     printf("Enter the Phone Number:");
-    scanf("%lld",&Students[TotalStudents].PhoneNo);
+    scanf("%lld",&Students[TotalStudents].PhoneNo);//Assign Student Father phone Number
     printf("Enter the Student class:");
-    scanf("%d",&Students[TotalStudents].Stuclass);
+    scanf("%d",&Students[TotalStudents].Stuclass);//Assign Student Class 
     printf("Enter the Age of the Student:");
-    scanf("%d",&Students[TotalStudents].age);
+    scanf("%d",&Students[TotalStudents].age);//Assign Student Age
     printf("Enter the Gender(M/F):");
-    scanf(" %c",&Students[TotalStudents].gender);
-    Students[TotalStudents].AdmissionTime=time(NULL);
-    int a=CheckDetails();
+    scanf(" %c",&Students[TotalStudents].gender);//Assign Student Gender
+    Students[TotalStudents].AdmissionTime=time(NULL);//Assign the Student Admission time
+    int a=CheckDetails();//calling the checkdetails function because if the valid details and also return the values
     if(a==0)
     {
         printf("Students Details are Correct\n");
